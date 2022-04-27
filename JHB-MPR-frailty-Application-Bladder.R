8346fbd9f66043f8ba202487f7553844297ab7d5
fatimZJ <fatimajaouimaa@gmail.com>
2022-04-27 17:42:16 +0100

    adding comments

--- JHB-MPR-frailty-Application-Bladder.R
+++ JHB-MPR-frailty-Application-Bladder.R
@@ -1,130 +1,166 @@
-################################################################################
-#                                                                              #
-# JHB-MPR-frailty-Code-Share                                                   #
-#                                                                              #
-################################################################################
+##############################################################################
+#                                                                            #
+# JHB-MPR-frailty-Code-Share                                                 #
+#                                                                            #
+##############################################################################
 
 # This R script file implements the h-likelihood procedure of 
 # "Multi-Parameter Regression Survival Modelling with Random Effects" 
-
+# by Fatima-Zahra Jaouimaa, Il Do Ha, and Kevin Burke
+#
 # The necessary functions are sourced from the github repository 
-# "https://github.com/fatimZJ/JHB-MPR-Frailty". A description of the purpose of 
-# each function is provided.
+# "https://github.com/fatimZJ/JHB-MPR-Frailty", wherein a description of the
+# purpose of each function can be found.
+#
+# The code below fits the BVNF model (bivariate normal frailties), the
+# ScF model (scale frailty) and ShF model (shape frailty) to the bladder
+# cancer dataset as per Section 4.3 of the paper.
+#
+# The code runs sequentially and requires the frailtyHL and mpr pacakges.
+# An internet connection is also needed for the purpose of loading in the
+# underlying R script from github.
 
-# The BVNF model (model assuming bivariate normal frailties), ScF model (model 
-# assuming scale frailty) and ShF model (model assuming shape frailty) are 
-# fitted to the Bladder Cancer dataset analysed in Section 4.3 of the paper.
 
-# The code runs cleanly sequentially (and requires installing the devtools, 
-# frailtyHL and mpr pacakges). 
+##############################################################################
+#                                                                            #
+# Application to the Bladder Cancer dataset                                  #
+#                                                                            #
+##############################################################################
 
-################################################################################
-#                                                                              #
-# Application to the Bladder Cancer dataset                                    #
-#                                                                              #
-################################################################################
+
+################ packages and github script
+
+## clear current environment
+rm(list = ls(all = TRUE))
+
 ## loading required packages
-library(devtools)
 library(frailtyHL)
 library(mpr)
 
 ## sources the functions from github
-SourceURL <- "https://raw.githubusercontent.com/fatimZJ/JHB-MPR-Frailty/main/JHB-MPR-frailty-Code-Share-BVN-ScF-ShF.R"
-source_url(SourceURL)
+url <- "https://raw.githubusercontent.com/fatimZJ/JHB-MPR-Frailty/main/JHB-MPR-frailty-Code-Share-BVN-ScF-ShF.R"
+script <- paste(readLines(url), collapse = "\n")
+eval(parse(text = script), envir= .GlobalEnv)
 
-## loading the dataset
+
+################ loading and manipulating data
+
+## load data
 data(bladder0, package = "frailtyHL")
 
-str(bladder0)
-## some data manipulation 
+## omit observations with zero survival time
 bladder <- bladder0[(bladder0$Surtime != 0), ]
 
-bladder[,c("Center", "Chemo", "Tustat")] <- lapply(bladder[,c("Center", "Chemo",
-                                                              "Tustat")], factor)
-str(bladder)
-# checking for NAs
-apply(bladder, 2, function(x) sum(is.na(x)))
+## making factor variables
+bladder[, c("Center", "Chemo", "Tustat")] <-
+  lapply(bladder[, c("Center", "Chemo", "Tustat")], factor)
 
-# changing the survival time from days to year
+## changing the survival time from days to year
 bladder$Surtime <- bladder$Surtime/365
 
-# model matrix
-X <- model.matrix(~Chemo + Tustat, data = bladder) 
+## display data sctructure
+str(bladder)
 
-# defining the data.frame that gets passed into the model fitting function- first
-# column is the survival time, the second is the censoring indicator, followed by 
-# the covariates, and the last column corresponds to the centre,
+################ initialization
 
-surdata <- data.frame(bladder$Surtime, bladder$Status, X, as.factor(bladder$Center))
+## model matrix
+X <- model.matrix(~ Chemo + Tustat, data = bladder) 
 
-# defining other parameters 
+## data is a data.frame used in the fitting function and whose columns
+## must have the following order: the first is the survival time, the second
+## is the censoring indicator, the next group are the covariates, and the
+## last column is the centre ID.
+data <- data.frame(bladder$Surtime, bladder$Status,
+                      X, as.factor(bladder$Center))
+
+## some constants that are needed
 k <- dim(X)[2]
 q <- nlevels(bladder$Center)
 maxiter <- 5000
 halfmax <- 500
 tol <- 1e-4
 
-## generating starting values 
-ti <- surdata[,1]
-deltai <- surdata[,2]
-lam.hat <- sum(deltai)/sum(ti)
+################ setting initial values
 
-bet0 <- log(lam.hat)
-alp0 <- 0.01
-init0 <- c(bet0, alp0)
+## this portion is used to get some "good" initial values that are used later
+## when fitting the frailty models
 
-# fitting standard no frailty, no covariate Weibull MPR model (to get starting values later)
-nocov.weibfit <- mpr(Surv(Surtime,Status)~list(~1,~1), data = bladder, init = init0)
+## exponential dist parameter estimate 
+exp_rate_mle <- sum(bladder$Status) / sum(bladder$Surtime)
 
-init <- c(nocov.weibfit$coefficients$beta, rep(0.01, (k - 1)), 
-          nocov.weibfit$coefficients$alpha, rep(0.01, (k - 1)))
+init0 <- c(log(exp_rate_mle), 0.01)
 
-# fitting a no frailty weibull MPR model with covariates
+# first fit a no frailty, no covariate Weibull model
+weibull <- mpr(Surv(Surtime, Status) ~ 1, data = bladder, init = init0)
 
-weib.fit <- mpr(Surv(Surtime, Status)~list(~Chemo + Tustat,~Chemo + Tustat), data = bladder, 
-                init = init)
+init0 <- c(weibull$coefficients$beta, rep(0.01, 2), 
+           weibull$coefficients$alpha, rep(0.01, 2))
 
-thetav.init <- c(weib.fit$coefficients$beta, weib.fit$coefficients$alpha, 
+# then fit a Weibull MPR model (but still there is no frailty)
+weibull_mpr <- mpr(Surv(Surtime, Status)~ Chemo + Tustat,
+                   data = bladder, init = init0)
+
+
+################ model fitting
+
+## BVNF model: Weibull MPR with bivariate normal frailties
+
+# initial values for fixed effects and frailties
+thetav_init <- c(weibull_mpr$coefficients$beta,
+                 weibull_mpr$coefficients$alpha, 
                  rep(0.01, (q*2)))
-disper.init <- c(0.1, 0.1, -0.1)
 
-## fitting the Weibull MPR model assuming BVN random effects- BVNF 
+# initial values for frailty dispersion parameters
+disper_init <- c(0.1, 0.1, -0.1)
 
-estimates.BVNF <- NA
+BVNF <- NA
 
-estimates.BVNF <- HLfit.algo(surdata = surdata, k = k, q = q, thetav.init = thetav.init, 
-                        tol = tol, disper.init = disper.init, maxiter = maxiter, 
-                        halfmax = halfmax, frailtystruc = "BVNF")
+BVNF <- mpr_frailty(data = surdata, frailty = "BVNF", k = k, q = q,
+                    thetav_init = thetav_init, disper_init = disper_init,  
+                    tol = tol, maxiter = maxiter, halfmax = halfmax)
 
-round(estimates.BVNF[[1]],2)
-round(estimates.BVNF[[2]],2)
-round(estimates.BVNF[[3]],2)
+## note that ".b" indicates the scale (beta) coefficients and ".a" indicates
+## the shape (alpha) coefficients
 
-## fitting the 
-thetav.init <- c(weib.fit$coefficients$beta, weib.fit$coefficients$alpha, 
+round(BVNF[[1]],2)
+round(BVNF[[2]],2)
+round(BVNF[[3]],2)
+
+
+## ScF model: Weibull MPR with scale frailties
+
+thetav_init <- c(weibull_mpr$coefficients$beta,
+                 weibull_mpr$coefficients$alpha, 
                  rep(0.01, q))
-disper.init <- 0.1
 
-##Fitting the Weibull MPR model assuming scale random effects model- ScF
-estimates.ScF <- NA
+disper_init <- 0.1
 
-estimates.ScF <- HLfit.algo(surdata = surdata, k = k, q = q, thetav.init = thetav.init, tol = tol,
-                   disper.init = disper.init, maxiter = maxiter, halfmax = halfmax,
-                   frailtystruc = "ScF")
+ScF <- NA
 
-round(estimates.ScF[[1]],2)
-round(estimates.ScF[[2]],2)
-round(estimates.ScF[[3]],2)
+ScF <- mpr_frailty(data = surdata, frailty = "ScF", k = k, q = q, 
+                  thetav_init = thetav_init, disper_init = disper_init,
+                  tol = tol, maxiter = maxiter,  halfmax = halfmax)
 
-##Fitting the Weibull MPR model assuming scale random effects model- ScF
-estimates.ShF <- NA
 
-estimates.ShF <- HLfit.algo(surdata = surdata, k = k, q = q, thetav.init = thetav.init, tol = tol,
-                   disper.init = disper.init, maxiter = maxiter, halfmax = halfmax,
-                   frailtystruc = "ShF")
+round(ScF[[1]],2)
+round(ScF[[2]],2)
+round(ScF[[3]],2)
 
-round(estimates.ShF[[1]],2)
-round(estimates.ShF[[2]],2)
-round(estimates.ShF[[3]],2)
+
+## ShF model: Weibull MPR with shape frailties
+
+ShF <- NA
+
+ShF <- mpr_frailty(data = surdata, frailty = "ShF", k = k, q = q, 
+                   thetav_init = thetav_init, disper_init = disper_init, 
+                   tol = tol, maxiter = maxiter, halfmax = halfmax)
+
+
+round(ShF[[1]],2)
+round(ShF[[2]],2)
+round(ShF[[3]],2)
+
+
+## END
 
 
